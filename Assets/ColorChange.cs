7aeb95565a19cb92d698bbc3681fdb60f96bf708
Fat'hah Noor Prawita <fathah.noor@yahoo.com>
2017-07-24 01:20:20 +0700

    Add Speech Recognition

---
+++ Assets/ColorChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour {
+
+	TextMesh textMesh;
+
+	// Use this for initialization
+	void Start () {
+		textMesh = GetComponent<TextMesh>();
+	}
+	
+	public void	ColorBlue(){
+		textMesh.color = Color.blue;
+	}
+
+	public void	ColorGreen(){
+		textMesh.color = Color.green;
+	}
+
+	public void	ColorRed(){
+		textMesh.color = Color.red;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
